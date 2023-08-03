f1371ced9a88a4f7f5757ee99eeee7b07d9188c0
nuuenkdaniel <lewdaniel2@gmail.com>
2023-08-02 20:54:01 -0400

    connected to db

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello")
+import mysql.connector
+import json 
+
+with open("config.json","r") as config:
+    dbPassword = (json.load(config)["dbPass"])
+
+db = mysql.connector.connect(
+   host = "localhost",
+   user = "root",
+   passwd = dbPassword,
+   database = "phone_directory"
+)
+
+print(db)
