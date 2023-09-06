fc329bcba86e986d66ca37419f4b01a0bfe0b863
nuuenkdaniel <lewdaniel2@gmail.com>
2023-09-06 12:13:51 -0400

    made toggleable filters

--- website/static/search.js
+++ website/static/search.js
@@ -1,38 +1,38 @@
 let html = "";
 $(document).ready(function() {
-    $('#filterOptions').on('change',function() {
+    $("#filterOptions").on("change",function() {
         html = "";
-        if(document.querySelector('#name-filter').checked) {
+        if(document.querySelector("#name-filter").checked) {
             html += '<div id="name-field">\
             <label for="name">Name*</label>\
             <input id="name" type="text" class="form-control" placeholder="Enter a name" required>\
             </div>';
         }
         else if(document.getElement) {
-            document.getElementById('name-field').remove();
+            document.getElementById("name-field").remove();
         }
 
-        if(document.querySelector('#email-filter').checked) {
+        if(document.querySelector("#email-filter").checked) {
             html += '<div id="email-field">\
             <label for="email">Email*</label>\
             <input id="email" type="email" class="form-control" placeholder="Enter an email" required>\
             </div>'
         }
-        else if(document.getElementById('email-field') !== null) {
-            document.getElementById('email-field').remove();
+        else if(document.getElementById("email-field") !== null) {
+            document.getElementById("email-field").remove();
         }
         
-        if(document.querySelector('#number-filter').checked) {
+        if(document.querySelector("#number-filter").checked) {
             html += '<div id="number-field">\
             <label for="number">Phone Number*</label>\
             <input id="number" type="tel" class="form-control" placeholder="Enter a phone number" required>\
             </div>'
         }
-        else if(document.getElementById('number-field') !== null) {
-            document.getElementById('number-field').remove();
+        else if(document.getElementById("number-field") !== null) {
+            document.getElementById("number-field").remove();
         }
 
-        if(document.querySelector('#address-filter').checked) {
+        if(document.querySelector("#address-filter").checked) {
             html += '<div id="address-field">\
             <label for="street-address">Street Address*</label>\
             <input id="street-address" type="text" class="form-control" placeholder="Enter the street address" required>\
@@ -44,10 +44,10 @@ $(document).ready(function() {
             <input id="zip" type="number" class="form-control" placeholder="Enter the zipcode" required>\
             </div>'
         }
-        else if(document.getElementById('address-field') !== null) {
-            document.getElementById('address-field').remove();
+        else if(document.getElementById("address-field") !== null) {
+            document.getElementById("address-field").remove();
         }
 
-        document.getElementById('search-fields').innerHTML = html;
+        document.getElementById("search-fields").innerHTML = html;
     });
 });
--- website/views.py
+++ website/views.py
@@ -1,31 +1,42 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 
-views = Blueprint('views',__name__)
+views = Blueprint("views",__name__)
 
 @views.route('/')
 def home():
     from website.database import search
-    print(search(["address"],["name"],["Daniel Lew"]))
+    # print(search(["address"],["name"],["Daniel Lew"]))
     return render_template("home.html")
 
-@views.route('/add-person', methods=['GET','POST'])
+@views.route("/add-person", methods=["GET","POST"])
 def add_person():
-    if request.method == 'POST':
+    if request.method == "POST":
         from website.database import add_person
-        email = request.form.get('email')
-        name = request.form.get('name')
-        phone_number = request.form.get('phone_number')
-        street_address = request.form.get('street_address')
-        city = request.form.get('city')
-        state = request.form.get('state')
-        zipcode = request.form.get('zip')
+        email = request.form.get("email")
+        name = request.form.get("name")
+        phone_number = request.form.get("phone_number")
+        street_address = request.form.get("street_address")
+        city = request.form.get("city")
+        state = request.form.get("state")
+        zipcode = request.form.get("zip")
         add_person(name,phone_number,street_address,city,state,zipcode,email)
         
     return render_template("add_person.html")
 
-@views.route('/search', methods=['GET','POST'])
-def search_by_name():
-    if request.method == 'POST':
+@views.route("/search", methods=["GET","POST"])
+def search():
+    if request.method == "POST":
         from website.database import search
-        request.form.get()
+        name = request.form.get("name")
+        email = request.form.get("email")
+        phone_number = request.form.get("number")
+        street_address = request.form.get("street-address")
+        city = request.form.get("city")
+        state = request.form.get("state")
+        zipcode = request.form.get("zip")
+        return redirect(url_for("views.results"))
     return render_template("search.html")
+
+@views.route("/results")
+def results():
+    return render_template("results.html")
